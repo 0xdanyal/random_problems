9e9937157c681fc94bdc2f317ca355d44c9f76c2
Danyal Khan <noreply.github.com>
2026-05-05 14:44:41 +0500

    added first probs

---
+++ reverse_string.js
@@ -0,0 +1,8 @@
+
+
+
+let string="hello"
+
+let reversed = string.split("").reverse().join("");
+
+console.log(reversed);
---
+++ swap_nums.js
@@ -0,0 +1,9 @@
+// swapping two numbers without using a 3rd variable
+
+
+let a = 3, b = 4;
+[a, b] = [b, a];
+
+
+console.log("value of a=", a)
+console.log("value of b=", b)
