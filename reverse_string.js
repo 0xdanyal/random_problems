35ac8468ecaf12e286e1bdebada130dfe7cbd16a
Danyal Khan <noreply.github.com>
2026-05-05 14:48:22 +0500

    manual code

--- reverse_string.js
+++ reverse_string.js
@@ -1,8 +1,19 @@
 
 
 
-let string="hello"
+// let string="hello"
 
-let reversed = string.split("").reverse().join("");
+// let reversed = string.split("").reverse().join("");
 
-console.log(reversed);
+// console.log(reversed);
+
+
+// no built-in methods - just logic building
+let str = "hello";
+let result = "";
+
+for (let i = str.length - 1; i >= 0; i--) {
+    result += str[i];
+}
+
+console.log(result);
