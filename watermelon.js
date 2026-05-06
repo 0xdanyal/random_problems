ec5a466b28b40090a83892eb1ff285f9537e7a45
Danyal Khan <noreply.github.com>
2026-05-06 23:23:59 +0500

    CF-800 level prob solved

---
+++ watermelon.js
@@ -0,0 +1,12 @@
+//                  Watermelon
+
+//  w must be EVEN and greater than 2 (not 2 cause 1+1 is still adding odds)
+
+
+function watermelon(w){
+
+    if(w%2===0 && w>2){
+        console.log("YES");
+    }
+    console.log("NO");
+}
